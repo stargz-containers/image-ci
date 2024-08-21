bac5d2c2a3770afacad6f244a0c555c7afc97244
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2024-08-21 12:30:11 +0900

    Bump up tools
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -2,13 +2,13 @@
 
 set -eu -o pipefail
 
-wget https://github.com/containerd/nerdctl/releases/download/v0.23.0/nerdctl-full-0.23.0-linux-amd64.tar.gz -O /tmp/nerdctl.tar.gz
-echo "2097ffb95c6ce3d847ca4882867297b5ab80e3daea6f967e96ce00cc636981b6  /tmp/nerdctl.tar.gz" | sha256sum -c
+wget https://github.com/containerd/nerdctl/releases/download/v1.7.6/nerdctl-full-1.7.6-linux-amd64.tar.gz -O /tmp/nerdctl.tar.gz
+echo "2c841e097fcfb5a1760bd354b3778cb695b44cd01f9f271c17507dc4a0b25606  /tmp/nerdctl.tar.gz" | sha256sum -c
 
 tar Cxzvvf /usr/local /tmp/nerdctl.tar.gz
 
-wget https://github.com/google/go-containerregistry/releases/download/v0.11.0/go-containerregistry_Linux_x86_64.tar.gz -O /tmp/go-containerregistry_Linux_x86_64.tar.gz
-echo "3cec40eb0fac2e6ed4b71de682ae562d15819ab92145e4f669b57baf04797adb /tmp/go-containerregistry_Linux_x86_64.tar.gz" | sha256sum -c
+wget https://github.com/google/go-containerregistry/releases/download/v0.20.2/go-containerregistry_Linux_x86_64.tar.gz -O /tmp/go-containerregistry_Linux_x86_64.tar.gz
+echo "c14340087103ba9dadf61d45acd20675490fd0ccbd56ac7901fc1b502137f44b /tmp/go-containerregistry_Linux_x86_64.tar.gz" | sha256sum -c
 
 tar Cxzvvf /usr/local/bin/ /tmp/go-containerregistry_Linux_x86_64.tar.gz
 
