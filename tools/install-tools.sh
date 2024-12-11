8c34120841547d98125729cf6ff698c67fb77612
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2024-12-11 10:54:43 +0900

    Bump up to nerdctl v2, containerd v2
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -2,14 +2,14 @@
 
 set -eu -o pipefail
 
-wget https://github.com/containerd/nerdctl/releases/download/v1.7.6/nerdctl-full-1.7.6-linux-amd64.tar.gz -O /tmp/nerdctl.tar.gz
-echo "2c841e097fcfb5a1760bd354b3778cb695b44cd01f9f271c17507dc4a0b25606  /tmp/nerdctl.tar.gz" | sha256sum -c
+wget https://github.com/containerd/nerdctl/releases/download/v2.0.3/nerdctl-full-2.0.3-linux-amd64.tar.gz -O /tmp/nerdctl.tar.gz
+echo "91bfb8faec1673f3e7c3a020812acffc50a7d7dd82019461f6cfa46435240903  /tmp/nerdctl.tar.gz" | sha256sum -c
 
-tar Cxzvvf /usr/local /tmp/nerdctl.tar.gz
+tar Cxzvvf /usr/ /tmp/nerdctl.tar.gz
 
 wget https://github.com/google/go-containerregistry/releases/download/v0.20.2/go-containerregistry_Linux_x86_64.tar.gz -O /tmp/go-containerregistry_Linux_x86_64.tar.gz
 echo "c14340087103ba9dadf61d45acd20675490fd0ccbd56ac7901fc1b502137f44b /tmp/go-containerregistry_Linux_x86_64.tar.gz" | sha256sum -c
 
-tar Cxzvvf /usr/local/bin/ /tmp/go-containerregistry_Linux_x86_64.tar.gz
+tar Cxzvvf /usr/bin/ /tmp/go-containerregistry_Linux_x86_64.tar.gz
 
 crane -h
--- tools/start-containerd.sh
+++ tools/start-containerd.sh
@@ -8,4 +8,5 @@ containerd &
 sleep 5
 
 # we verify that it works
+nerdctl version
 nerdctl run --rm ghcr.io/stargz-containers/ubuntu:22.04 echo hello
